74a44dfdbf855075b6f0737ea0219b22bfe74c63
Maria210194 <mary.stachi@gmail.com>
2022-09-10 00:28:47 +0200

    Init + doll's first rotations

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+renderer.setClearColor( 0xb7c3f3, 1)
+
+const light = new THREE.AmbientLight( 0xffffff ); 
+scene.add( light );
+
+
+camera.position.z = 5;
+
+
+const loader = new THREE.GLTFLoader()
+
+
+class Doll{
+    constructor(){
+        loader.load("../models/scene.gltf", (gltf) => {
+            scene.add (gltf.scene);
+            gltf.scene.scale.set(.4, .4, .4);
+            gltf.scene.position.set(0, -1, 0);
+            this.doll = gltf.scene;
+        })
+    }
+    lookBackward(){
+        gsap.to(this.doll.rotation, {y: -3.15, duration: .45})
+    }
+    loolForward(){
+        gsap.to(this.doll.rotation, {x: 0, duration: .45})
+    }
+}
+
+let doll = new Doll();
+setTimeout(()=> {
+    doll.lookBackward();
+}, 3000);
+
+function animate() {
+    renderer.render( scene, camera );
+	requestAnimationFrame( animate );
+}
+animate();
+
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight)
+}
+
