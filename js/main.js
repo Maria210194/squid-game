b60ec67567f71aab76ad5b4f60da8db1facb790c
Maria210194 <mary.stachi@gmail.com>
2022-09-11 01:13:52 +0200

    Add game logic + Done

--- js/main.js
+++ js/main.js
@@ -5,17 +5,46 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-renderer.setClearColor( 0xb7c3f3, 1)
-
 const light = new THREE.AmbientLight( 0xffffff ); 
 scene.add( light );
 
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.3 );
+directionalLight.castShadow = true
+scene.add( directionalLight )
+directionalLight.position.set( 0, 1, 1 )
 
 camera.position.z = 5;
+renderer.setClearColor( 0xf7ede2, 1);
 
 
-const loader = new THREE.GLTFLoader()
 
+// global variables
+const start_position = 3;
+const end_position = -start_position;
+const text = document.querySelector(".text");
+const TIME_LIMIT = 10;
+let gameStat = "loading";
+let isLookingBackward = true;
+const startBtn = document.querySelector('.start-btn');
+
+
+
+function createCube(size, positionX, rotY = 0, color = 0x9e0059){
+    const geometry = new THREE.BoxGeometry( size.w, size.h, size.d);
+    const material = new THREE.MeshBasicMaterial( { color: color } );
+    const cube = new THREE.Mesh( geometry, material );
+    cube.position.x = positionX;
+    cube.rotation.y = rotY;
+    scene.add( cube );
+    return cube
+}
+
+
+const loader = new THREE.GLTFLoader();
+
+function delay(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 class Doll{
     constructor(){
@@ -24,24 +53,126 @@ class Doll{
             gltf.scene.scale.set(.4, .4, .4);
             gltf.scene.position.set(0, -1, 0);
             this.doll = gltf.scene;
+            startBtn.innerText = "start"
         })
     }
     lookBackward(){
-        gsap.to(this.doll.rotation, {y: -3.15, duration: .45})
+        gsap.to(this.doll.rotation, {y: -3.15, duration: .45});
+        setTimeout(()=> isLookingBackward = true, 150);
+    };
+    lookForward(){
+        gsap.to(this.doll.rotation, {y: 0, duration: .45});
+        setTimeout(()=> isLookingBackward = false, 450);
     }
-    loolForward(){
-        gsap.to(this.doll.rotation, {x: 0, duration: .45})
+
+    async start(){
+        this.lookBackward();
+        await delay((Math.random()* 1000) + 1000);
+        this.lookForward();
+        await delay((Math.random()* 750) + 750);
+        this.start();
     }
 }
 
+
 let doll = new Doll();
-setTimeout(()=> {
-    doll.lookBackward();
-}, 3000);
+
+
+function createTrack(){
+    createCube({w:start_position * 2 + .2, h:1.5, d:1}, 0, 0, 0x9e0059).position.z = -1;
+    createCube({w:.2, h:1.5, d:1}, start_position, -.35);
+    createCube({w:.2, h:1.5, d:1}, end_position, .35);
+}
+createTrack();
+
+
+// player's class
+class Player{
+    constructor(){
+        const geometry = new THREE.SphereGeometry( .2, 32, 16 );
+        const material = new THREE.MeshBasicMaterial( { color: 0x0b090a } );
+        const sphere = new THREE.Mesh( geometry, material );
+        sphere.position.z = 1;
+        sphere.position.x = start_position
+        scene.add( sphere );
+        this.player = sphere;
+        this.playerInfo = {
+            positionX: start_position -.4, 
+            velocity: 0
+        }
+    }
+
+    run(){
+        this.playerInfo.velocity = .03
+    }
+
+    stop(){
+        gsap.to(this.playerInfo, {velocity: 0, duration: .3})
+    }
+
+    check(){
+        if(this.playerInfo.velocity > 0 && !isLookingBackward){
+            text.innerText = "You lose..";
+            gameStat = "over";
+        }
+        if(this.playerInfo.positionX < end_position + .4){
+            text.innerText = "You win!!";
+            gameStat = "over";
+        }
+    }
+
+    update(){
+        this.check();
+        this.playerInfo.positionX -= this.playerInfo.velocity;
+        this.player.position.x = this.playerInfo.positionX
+    }
+}
+
+const player = new Player();
+
+
+// GAME LOGIC
+async function init() {
+    await delay(500);
+    text.innerText = "Start in 3";
+    await delay(500);
+    text.innerText = "Start in 2";
+    await delay(500);
+    text.innerText = "Start in 1";
+    await delay(500);
+    text.innerText = "Goo!!";
+    startGame();
+};
+
+function startGame() {
+    gameStat = "started"
+    let progressBar = createCube({w:5, h:.1, d:1}, 0);
+    progressBar.position.y = 3.35;
+    gsap.to(progressBar.scale, {x:0, duration: TIME_LIMIT, ease: "none"});
+    doll.start();
+    setTimeout(()=> {
+        if(gameStat !== "over"){
+            text.innerText = "You ran out of time!"
+            gameStat = "over"
+        }
+    }, TIME_LIMIT * 1000)
+}
+
+// init();
+
+startBtn.addEventListener('click', () => {
+    if(startBtn.innerText == "START"){
+        init()
+        document.querySelector('.modal').style.display = "none"
+    }
+})
+
 
 function animate() {
+    if(gameStat == "over") return
     renderer.render( scene, camera );
 	requestAnimationFrame( animate );
+    player.update();
 }
 animate();
 
@@ -53,3 +184,16 @@ function onWindowResize(){
     renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
+
+window.addEventListener('keydown', (e) => {
+    if(gameStat != "started") return
+    if(e.key == "ArrowUp"){
+        player.run()
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    if(e.key == "ArrowDown"){
+        player.stop()
+    }
+})
